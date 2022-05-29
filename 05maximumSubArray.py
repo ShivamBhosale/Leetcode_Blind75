e502f4370ca5552a9f1e7936abdfa68a13443577
Shivam Bhosale <50315818+ShivamBhosale@users.noreply.github.com>
2022-05-29 21:56:49 +0530

    Maximum Subbarray

---
+++ 05maximumSubArray.py
@@ -0,0 +1,11 @@
+def maxSubArray(nums):
+    maxSub = nums[0]
+    curSub = 0
+    for i in nums:
+        if curSub < 0:
+            curSub = 0
+        curSub += i
+        maxSub = max(maxSub, curSub)
+    return maxSub
+
+print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
